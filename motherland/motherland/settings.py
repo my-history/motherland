fd80bd52eb305c7f7391b24b0fc3c8567543319c
Daniil <dadyarri@gmail.com>
2020-07-11 13:24:57 +0300

    Register applications.

--- motherland/motherland/settings.py
+++ motherland/motherland/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'base',
+    'blog'
 ]
 
 MIDDLEWARE = [
@@ -99,6 +101,8 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
+
+AUTH_USER_MODEL = 'base.BaseUser'
 
 
 # Internationalization
