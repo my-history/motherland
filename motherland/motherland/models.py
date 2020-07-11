2f7acb13adb474dc3129f37787a6b921ea786733
Daniil <dadyarri@gmail.com>
2020-07-11 12:12:50 +0300

    Add template of BaseUser model.

--- motherland/motherland/models.py
+++ motherland/motherland/models.py
@@ -1,10 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+
+class BaseUser(User):
+    rating = models.IntegerField(verbose_name="User's rating")
+    # posts = None  # many-to-many to Post
+    # comments = None  # many-to-many to Comment
 
 
 class Post(models.Model):
     id = models.IntegerField(primary_key=True)
     # author = None  # User
-    date = models.DateTimeField(verbose_name="Datetime of creating post")
+    date = models.DateTimeField(verbose_name="Post creation datetime")
     title = models.TextField(verbose_name="Post's title")
     content_raw = models.TextField(verbose_name="Post's markdown markup")
     # comments = None  # many-to-many к Comment
