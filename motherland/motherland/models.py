a4ad6c0b8045ab59d61c3398f513747ad1c34c80
Daniil <dadyarri@gmail.com>
2020-07-11 12:11:35 +0300

    Add verbose names to fields in Post model.

--- motherland/motherland/models.py
+++ motherland/motherland/models.py
@@ -4,8 +4,8 @@ from django.db import models
 class Post(models.Model):
     id = models.IntegerField(primary_key=True)
     # author = None  # User
-    date = models.DateTimeField()
-    title = models.TextField()
-    content_raw = models.TextField()
+    date = models.DateTimeField(verbose_name="Datetime of creating post")
+    title = models.TextField(verbose_name="Post's title")
+    content_raw = models.TextField(verbose_name="Post's markdown markup")
     # comments = None  # many-to-many к Comment
-    rating = models.IntegerField()
+    rating = models.IntegerField(verbose_name="Rating of post")
