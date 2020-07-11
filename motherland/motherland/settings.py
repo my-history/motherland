4eb98c14a690cfeea60c7d904b8a391b7fcb99a9
Daniil <dadyarri@gmail.com>
2020-07-11 21:05:58 +0300

    Move configs to .env.

--- motherland/motherland/settings.py
+++ motherland/motherland/settings.py
@@ -20,10 +20,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'kek=!d3%wac)=oxa@0&n_a^2888igk!_9wpi=b20^#$bvk#2-r'
+SECRET_KEY = os.environ["SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ["PRODUCTION"]
 
 ALLOWED_HOSTS = []
 
